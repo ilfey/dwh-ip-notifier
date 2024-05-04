f99d2c3c0ccf6ed921e26df92c313e24982e6524
ilfey <ilfey.global@gmail.com>
2024-05-04 16:58:14 +0700

    add configuration and scheduling

---
+++ README.md
@@ -0,0 +1,12 @@
+# Discord webhook IP notifier
+
+A program that monitors device IP and sends a message to discord when it changes.
+
+## Configuration
+
+Create `.env` file like this:
+
+```env
+WEBHOOK_URL=https://discord.com/api/webhooks/1235998390282620928/YMhil0KH16QKk0GdzjgUsmYJi1euFWStQqU_kdePaiRcWqGhe_ZayioaL1_CfDajkpY4
+DELAY=1h
+```
--- main.go
+++ main.go
@@ -4,28 +4,80 @@ import (
 	"fmt"
 	"main/ipinfo"
 	"main/webhook"
+
+	"github.com/robfig/cron/v3"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	info, err := ipinfo.GetInfo()
-	if err != nil {
-		panic(err)
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+
+	viper.SetDefault("DELAY", "1h")
+
+	delay := viper.GetString("DELAY")
+	wh_url := viper.GetString("WEBHOOK_URL")
+	if wh_url == "" {
+		panic("WEBHOOK_URL is not set")
 	}
 
-	title := "New ip detected"
-	description := fmt.Sprintf("||`%s`||", info.IP)
+	errChan := make(chan error)
+	runner := cron.New()
 
-	msg := &webhook.Message{
-		Embeds: &[]webhook.Embed{
-			{
-				Title:       &title,
-				Description: &description,
+	var prevInfo *ipinfo.Info
+
+	// Add cron job
+	_, err := runner.AddFunc("@every "+delay, func() {
+		// Get info
+		info, err := ipinfo.GetInfo()
+		if err != nil {
+			errChan <- err
+			return
+		}
+
+		// Set default embed title and description
+		title := "New ip detected"
+		description := fmt.Sprintf("||`%s`||", info.IP)
+
+		// Check info is not exist
+		if prevInfo == nil {
+			title = "Started"
+		}
+
+		// Check info is changed
+		if prevInfo != nil && prevInfo.IP == info.IP {
+			return
+		}
+
+		msg := &webhook.Message{
+			Embeds: &[]webhook.Embed{
+				{
+					Title:       &title,
+					Description: &description,
+				},
 			},
-		},
-	}
+		}
 
-	err = webhook.SendMessage("https://discord.com/api/webhooks/1235998390282620928/YMhil0KH16QKk0GdzjgUsmYJi1euFWStQqU_kdePaiRcWqGhe_ZayioaL1_CfDajkpY4", msg)
+		// Send message
+		err = webhook.SendMessage(wh_url, msg)
+		if err != nil {
+			errChan <- err
+			return
+		}
+
+		// Save info
+		prevInfo = info
+	})
 	if err != nil {
 		panic(err)
+	}
+
+	runner.Start()
+
+	for {
+		err := <-errChan
+		if err != nil {
+			panic(err)
+		}
 	}
 }
